0d08e86a9da587f57503761cbac31e0252c8d4cc
cynen <cynen@foxmail.com>
2018-09-10 23:07:25 +0800

    WordTest
    只使用freemarker.jar包进行wrod模板操作.
    生成word文件.

---
+++ WordTest/WordTest/src/main/java/com/myth/WordTest/App.java
@@ -0,0 +1,36 @@
+package com.myth.WordTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Map<String, Object> map = new HashMap<String,Object>();
+        map.put("name", "蔡能");
+        map.put("sex", "男");
+        map.put("age", "18");
+        map.put("addr", "广东");
+        map.put("phone", "189");
+        map.put("yourname", "蔡能2");
+        map.put("year", "2018");
+        map.put("month", "12");
+        map.put("date", "11");
+        
+        
+        try {
+			WordUtils.creatWord(map);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.out.println("根据word模板生成word完成");
+    }
+}
---
+++ WordTest/WordTest/src/main/java/com/myth/WordTest/WordUtils.java
@@ -0,0 +1,68 @@
+package com.myth.WordTest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Map;
+
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class WordUtils {
+    //配置信息,代码本身写的还是很可读的,就不过多注解了
+    private static Configuration configuration = null;
+    //这里注意的是利用WordUtils的类加载器动态获得模板文件的位置
+    // 实际加载的src目录下的.
+    private static final String templateFolder =  WordUtils.class.getClassLoader().getResource("./").getPath();
+    static {
+    	// 设置版本.
+    	configuration = new Configuration(Configuration.VERSION_2_3_23);
+    	configuration.setDefaultEncoding("utf-8");
+        try {
+        	// 设置模板路径文件夹.
+            configuration.setDirectoryForTemplateLoading(new File(templateFolder));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+   }
+    public static void creatWord(Map map) throws Exception {
+    	// 具体的模板对象.
+        Template freemarkerTemplate = configuration.getTemplate("demo.ftl");
+        File file = null;
+        try {
+            // 调用工具类的createDoc方法生成Word文档
+            file = createDoc(map,freemarkerTemplate);
+        } finally {
+           //  if(file != null) file.delete(); // 删除临时文件
+        }
+    }
+ 
+    /**
+     * 生成word 数据.
+     * 
+     * @param dataMap
+     * @param template
+     * @return
+     * @throws Exception
+     */
+    private static File createDoc(Map<?, ?> dataMap, Template template) throws Exception {
+        String name =  "F:/test2.doc";
+        File f = new File(name);
+        if(!f.exists()) {
+        	f.createNewFile();
+        }
+        Template t = template;
+        try {
+            // 这个地方不能使用FileWriter因为需要指定编码类型否则生成的Word文档会因为有无法识别的编码而无法打开
+            Writer w = new OutputStreamWriter(new FileOutputStream(f), "utf-8");
+            t.process(dataMap, w);
+            w.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return f;
+    }
+}
