facd0343f9fdc1025991fa5ed85649718dae58b8
cynen <cynen@foxmail.com>
2018-10-31 21:05:29 +0800

    Mytomcat

--- MyTomcat/src/com/myth/test/SocketTest.java
+++ MyTomcat/src/com/myth/test/SocketTest.java
@@ -1,10 +1,22 @@
 package com.myth.test;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * 
+ * 
+*<p> Title: SocketTest  </p>
+*<p> Description: 
+*	自定义实现Tomcat的启动类.
+*</p>
+*<p> Copyright: Copyright (c) 2018</p>  
+*<p> Company: myth</p>  
+* @author myth
+* @date 2018年10月31日  
+* @version v1.0
+ */
 public class SocketTest {
 	
 	
